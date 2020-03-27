2ec359a8e055d2980f0e86e93f34c5fa73691134
alireza dizaji <alirezadizaji@yahoo.com>
2020-03-27 16:26:15 +0430

    Mapbox api with ProgressFragment added.

--- app/src/main/java/com/example/practice1/MainActivity.java
+++ app/src/main/java/com/example/practice1/MainActivity.java
@@ -1,18 +1,46 @@
 package com.example.practice1;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentActivity;
+import androidx.fragment.app.FragmentManager;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.ThreadPoolExecutor;
 
 public class    MainActivity extends AppCompatActivity {
+    ExecutorService service = Executors.newFixedThreadPool(5);
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        s
+        clickSearchCityButton(this);
+    }
+
+    public void clickSearchCityButton(final Context context) {
+        final Button button = findViewById(R.id.SearchCityButton);
+        final EditText text = findViewById(R.id.SearchCityInput);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ProgressFragment fragment = new ProgressFragment();
+                fragment.show(getSupportFragmentManager(), "hello");
+                service.execute(new SearchCityResult(button, text, context, fragment));
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/example/practice1/ProgressFragment.java
@@ -0,0 +1,20 @@
+package com.example.practice1;
+
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+
+import androidx.fragment.app.DialogFragment;
+
+public class ProgressFragment extends DialogFragment {
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        LayoutInflater inflater = getActivity().getLayoutInflater();
+        View view = inflater.inflate(R.layout.fragment_progress, null);
+        builder.setView(view);
+        return builder.create();
+    }
+}
